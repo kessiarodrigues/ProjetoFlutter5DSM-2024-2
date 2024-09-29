850bbc13a425db1f1fd4a164c5a0ee902733f6d1
Késsia Rodrigues Borges <kessia.rodrigues31@gmail.com>
2024-09-29 18:21:01 -0300

    trabalho

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:flutter/widgets.dart';
+import 'package:intl/intl.dart';
 
 void main() => runApp(const Banco());
 
@@ -11,7 +9,18 @@ class Banco extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ListaTransferencia(),
+      theme: ThemeData(
+          primarySwatch: Colors.pink,
+          appBarTheme: AppBarTheme(
+              backgroundColor: Colors.pinkAccent,
+              titleTextStyle: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold)),
+          buttonTheme: ButtonThemeData(buttonColor: Colors.pink),
+          floatingActionButtonTheme:
+              FloatingActionButtonThemeData(backgroundColor: Colors.pink[700])),
+      home: Dashboard(),
     );
   }
 }
@@ -32,7 +41,6 @@ class FormularioTransferencia extends StatelessWidget {
           "Nova Transferência",
           style: TextStyle(color: Colors.white),
         ),
-        backgroundColor: Colors.blue,
       ),
       body: Column(
         children: [
@@ -53,9 +61,6 @@ class FormularioTransferencia extends StatelessWidget {
                 _controllerCampoValor,
               );
             },
-            style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all(Colors.blue),
-            ),
             child: const Text(
               'Confirmar',
               style: TextStyle(color: Colors.white, fontSize: 18),
@@ -134,7 +139,6 @@ class ListaTransferenciasState extends State<ListaTransferencia> {
           "Transferência",
           style: TextStyle(color: Colors.white),
         ),
-        backgroundColor: Colors.blue,
       ),
       body: ListView.builder(
           itemCount: widget._transferencias.length,
@@ -156,7 +160,6 @@ class ListaTransferenciasState extends State<ListaTransferencia> {
             });
           });
         },
-        backgroundColor: Colors.blue,
         child: const Icon(
           Icons.add_circle_rounded,
           size: 35,
@@ -177,7 +180,14 @@ class ItemTransferencia extends StatelessWidget {
     return Card(
       child: ListTile(
         leading: const Icon(Icons.monetization_on, color: Colors.green),
-        title: Text(_transferencia.valor.toString()),
+        title: Text(
+          NumberFormat.currency(locale: 'pt_BR', symbol: 'R\$')
+              .format(_transferencia.valor),
+          style: TextStyle(
+            fontSize: 16.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
         subtitle: Text(_transferencia.numeroConta.toString()),
       ),
     );
@@ -193,5 +203,331 @@ class Transferencia {
   @override
   String toString() {
     return 'Transferência{valor: $valor, numeroConta: $numeroConta}';
+  }
+}
+
+class Dashboard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dashboard'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            height: 120,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: <Widget>[
+                _FeatureItem(
+                  'Transferência',
+                  Icons.monetization_on,
+                  onClick: () => _showTransferenciasList(context),
+                ),
+                _FeatureItem(
+                  'Contatos',
+                  Icons.people,
+                  onClick: () => _showContatosList(context),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _showTransferenciasList(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => ListaTransferencia(),
+      ),
+    );
+  }
+
+  void _showContatosList(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => ListaContatos(),
+      ),
+    );
+  }
+}
+
+class _FeatureItem extends StatelessWidget {
+  final String name;
+  final IconData icon;
+  final Function onClick;
+
+  _FeatureItem(this.name, this.icon, {required this.onClick});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Material(
+        color: Theme.of(context).primaryColor,
+        child: InkWell(
+          onTap: () => onClick(),
+          child: Container(
+            padding: EdgeInsets.all(8.0),
+            width: 150,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Icon(
+                  icon,
+                  color: Colors.white,
+                  size: 24.0,
+                ),
+                Text(
+                  name,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16.0,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ListaContatos extends StatefulWidget {
+  @override
+  _ListaContatosState createState() => _ListaContatosState();
+}
+
+class _ListaContatosState extends State<ListaContatos> {
+  final List _contatos = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Contatos'),
+      ),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          final contato = _contatos[index];
+          return _ContatoItem(contato);
+        },
+        itemCount: _contatos.length,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context)
+              .push(
+            MaterialPageRoute(
+              builder: (context) => FormularioContato(),
+            ),
+          )
+              .then((novoContato) {
+            if (novoContato != null) {
+              setState(() {
+                _contatos.add(novoContato);
+              });
+            }
+          });
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class _ContatoItem extends StatelessWidget {
+  final contato;
+
+  _ContatoItem(this.contato);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      child: ExpansionTile(
+        leading: CircleAvatar(
+          backgroundColor: Theme.of(context).primaryColor,
+          child: Icon(
+            Icons.person,
+            color: Colors.white,
+          ),
+        ),
+        title: Text(
+          contato.nome,
+          style: TextStyle(
+            fontSize: 18.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        subtitle: Text(
+          contato.telefone,
+          style: TextStyle(
+            fontSize: 14.0,
+          ),
+        ),
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                _buildInfoRow('Endereço', contato.endereco),
+                _buildInfoRow('E-mail', contato.email),
+                _buildInfoRow('CPF', contato.cpf),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildInfoRow(String label, String value) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            width: 80,
+            child: Text(
+              '$label:',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 14.0,
+              ),
+            ),
+          ),
+          Expanded(
+            child: Text(
+              value,
+              style: TextStyle(fontSize: 14.0),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class FormularioContato extends StatefulWidget {
+  @override
+  _FormularioContatoState createState() => _FormularioContatoState();
+}
+
+class _FormularioContatoState extends State<FormularioContato> {
+  final TextEditingController _nomeController = TextEditingController();
+  final TextEditingController _enderecoController = TextEditingController();
+  final TextEditingController _telefoneController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _cpfController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Novo contato'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              controller: _nomeController,
+              decoration: InputDecoration(
+                labelText: 'Nome completo',
+              ),
+              style: TextStyle(
+                fontSize: 24.0,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: TextField(
+                controller: _enderecoController,
+                decoration: InputDecoration(
+                  labelText: 'Endereço',
+                ),
+                style: TextStyle(
+                  fontSize: 24.0,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: TextField(
+                controller: _telefoneController,
+                decoration: InputDecoration(
+                  labelText: 'Telefone',
+                ),
+                style: TextStyle(
+                  fontSize: 24.0,
+                ),
+                keyboardType: TextInputType.phone,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: TextField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                  labelText: 'E-mail',
+                ),
+                style: TextStyle(
+                  fontSize: 24.0,
+                ),
+                keyboardType: TextInputType.emailAddress,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: TextField(
+                controller: _cpfController,
+                decoration: InputDecoration(
+                  labelText: 'CPF',
+                ),
+                style: TextStyle(
+                  fontSize: 24.0,
+                ),
+                keyboardType: TextInputType.number,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 16.0),
+              child: SizedBox(
+                width: double.maxFinite,
+                child: ElevatedButton(
+                  child: Text('Criar'),
+                  onPressed: () {
+                    final String nome = _nomeController.text;
+                    final String endereco = _enderecoController.text;
+                    final String telefone = _telefoneController.text;
+                    final String email = _emailController.text;
+                    final String cpf = _cpfController.text;
+
+                    final novoContato = (
+                      nome: nome,
+                      endereco: endereco,
+                      telefone: telefone,
+                      email: email,
+                      cpf: cpf,
+                    );
+                    Navigator.pop(context, novoContato);
+                  },
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
